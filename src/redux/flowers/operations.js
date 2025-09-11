049c40cb93ffd71de5dba89d873aba58f8fc2de6
Taras <r.taras.t.1@gmail.com>
2025-09-11 16:20:19 +0300

    updaate

--- src/redux/flowers/operations.js
+++ src/redux/flowers/operations.js
@@ -1,1 +1,16 @@
-const 
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://flowers-back-0xk7.onrender.com/";
+
+export const getFlowers = createAsyncThunk(
+  "flowers/getAllFlowers",
+  async (searchParams, thunkAPI) => {
+    try {
+      const response = await axios.get("/", { params: searchParams });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
--- src/redux/flowers/slice.js
+++ src/redux/flowers/slice.js
@@ -1,12 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getFlowers } from "./operations";
 
-const initialState = {};
+const initialState = {
+  hits: [],
+  page: 1,
+  perPage: 12,
+  totalPages: 0,
+  hasPreviousPage: false,
+  hasNextPage: false,
+  totalItems: 0,
+  isLoading: false,
+};
 
 const flowersSlice = createSlice({
   name: "flowers",
   initialState,
   reducers: {},
-  // extraReducers: (builder) => builder.addCase({}),
+  extraReducers: (builder) => {
+    builder.addCase(getFlowers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+  },
 });
 
 export const flowersReducer = flowersSlice.reducer;
